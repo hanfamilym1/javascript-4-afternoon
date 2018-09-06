07fe1abc6642e089d32f2051eb580c06cc57fb6d
hanfamilym1 <michael@hanfamily.com>
2018-09-06 16:20:42 -0600

    Finished

--- closures.js
+++ closures.js
@@ -23,12 +23,13 @@ function outer() {
 */
   
 // Code Here
-
+let inner = outer()
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+inner()
 
 
 
@@ -52,7 +53,9 @@ function callFriend(name) {
 */
 
 //Code Here
+let callJake = callFriend('Jake')
 
+callJake('435-555-9248')
 
 
 ////////// PROBLEM 3 //////////
@@ -62,15 +65,23 @@ function callFriend(name) {
 */
 
 //Code Here
+function makeCounter(){
+  let num = 0
+  return function(){
+    num++
+    return num
+  }
+
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,17 +98,19 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+    
   return {
+    inc: ()=>value+=1,
+    dec: ()=>value-=1
 
-  };
+  }; 
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +126,12 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+    function message(){
+      return `${welcomeText} ${firstname} ${lastname}.` 
+    }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,8 +160,13 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: ()=>privateMethod()
   };
 })();
+
+module.publicMethod()
+
+
 
 
 
@@ -163,6 +184,8 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: (num)=> secret+=num,
+    takeAwayFromSecret: (num)=>secret-=num
   };
 }
 
@@ -188,9 +211,8 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    setTimeout(function(x){return function() {
+      console.log(x)}}(i), i * 1000);
   }
 }
 timeOutCounter();
--- constructors.js
+++ constructors.js
@@ -14,7 +14,10 @@
 */
 
 // Code here
-
+function CarFactory (make, model){
+  this.make = make,
+  this.model = model
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,6 +36,7 @@ function Employee(name, email, hireDate) {
 */
 
 // Code here
+let bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
 
 
 
@@ -54,5 +58,16 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 */
 
 // Code here
+function Car(make, model, year){
+  
+  this.make = make,
+  this.model = model,
+  this.year = year
+
+  return {
+    moveCar: ()=>this.move+=10
+  }
+}
+
 
 
